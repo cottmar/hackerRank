f089482828798835ba101cbd7ee69586ee41fea9
cara ottmar <cara.ottmar@gmail.com>
2018-07-27 19:11:46 -0700

    comments added

--- hackerrank2.js
+++ hackerrank2.js
@@ -5,6 +5,12 @@ function simpleArraySum(ar) {
   return(ar.reduce(reducer));
 }
 
+// The first input array contains the correct answers to an exam, like ["a", "a", "b", "d"]. The second one is "answers" array and contains student's answers.
+
+// The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer(empty string).
+
+// If the score < 0, return 0.
+
 function checkExam(array1, array2) {
   var score = 0;
   for(var i = 0; i < array1.length; i++){
